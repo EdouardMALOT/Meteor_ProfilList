d360c48e12a9319f1e620bbd904f92284eaa5900
Edouard MLT <edouard.malot@gmail.com>
2017-02-07 12:44:42 +0100

    add fake datas in the DB

--- client/main.js
+++ client/main.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import EmployeeList from './components/employee_list.js';
 
 const App = () => {
   return(
-    <div>Hello Aix</div>
+    <EmployeeList />
   );
 }
 
--- server/main.js
+++ server/main.js
@@ -1,5 +1,25 @@
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employee.js';
+import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  //Check is there is already datas in the DB
+    const numberRecord = Employees.find({}).count();
+    console.log("Nb records", numberRecord);
+
+    if(!numberRecord) {
+      _.times(5000, () => {
+
+        //Generate Card
+          const { name, email, phone } = helpers.createCard();
+
+        //Add it to the DB
+          Employees.insert({   name, email, phone, avatar: image.avatar() });
+          
+      });
+    }
+
 });
