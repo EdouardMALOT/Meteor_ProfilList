aa416f0e6f0fadec86c81093fbdec84af86ec18a
Edouard MLT <edouard.malot@gmail.com>
2017-02-08 22:14:34 +0100

    Add search bar (html only)

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -4,7 +4,7 @@ import { Employees} from '../../imports/collections/employee';
 import EmployeeDetail from './employee_detail';
 
 
-const PER_PAGE = 20;
+const PER_PAGE = 10;
 
 class EmployeeList extends React.Component {
 
@@ -19,9 +19,27 @@ class EmployeeList extends React.Component {
         console.log('nb clicked = ', this.nbTimeCliked);
     }
 
+    submitHandler(event) {
+        event.preventDefault();
+        Meteor.call('link.insert', this.refs.input.value, (error) => {
+            if(error){
+                this.setState({ error: 'You url is not valid'});            //Display error msg
+            }else{
+                this.setState({ error: ''});                                //Reset error msg
+                this.refs.input.value = '';
+            }
+        });
+    }
+
     render() {
         return(
             <div>
+                <div style={{height:30}}></div>
+                <form onSubmit={this.submitHandler.bind(this)} className="form">
+                   <input ref="input" className="form-control" />
+                   <button className="btn btn-primary">Search</button>
+                </form>
+                <div style={{height:30}}></div>
 
                 <div className="employee-list"> 
                     {this.props.employees.map(employee =>  
